a6d8691f98cb8baa45c40aca79a4ebb2e1df97b2
Akashdeep Goel <akashdeep.goel1996@gmail.com>
2015-10-12 22:03:24 +0530

    Rendered Problems

--- Codemania/views.py
+++ Codemania/views.py
@@ -8,12 +8,10 @@ from django.template import RequestContext, loader
 
 def index(request):
 	problems = Problem.objects.order_by('added_at')
-	template = loader.get_template('Codemania/index.html')
 	context = RequestContext(request, {'problems':problems,})
-	return HttpResponse(template.render(context))
+	return render(request,"Codemania/index.html",context)
 def get_problem(request, title):
 	problem = Problem.objects.filter(title = title)
-	template = loader.get_template('Codemania/problems/problem.html')
+	#template = loader.get_template('Codemania/problems/problem.html')
 	context = RequestContext(request, {'problem':problem,})
-	return HttpResponse(template.render(context))
-
+	return render(request,"Codemania/problems/problem.html",context)
