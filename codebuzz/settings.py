2ed17c23bc462f815b97e99e03b0b9dbb5f1f7b4
Akashdeep Goel <akashdeep.goel1996@gmail.com>
2015-10-13 05:20:50 +0530

    Ptch:Fixed minor typo

--- codebuzz/settings.py
+++ codebuzz/settings.py
@@ -106,7 +106,7 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_r
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static_in_pro", "our_static"),
         #os.path.join(BASE_DIRS, "static_in_env")
-        #'/var/www/static',
+        #'/var/www/static',	
     )
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
