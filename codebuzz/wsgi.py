d5830b283b1ac7d5cca47b6b92ee71e750ebc937
Akashdeep Goel <akashdeep.goel1996@gmail.com>
2015-10-12 17:06:58 +0530

    Setup:Basic Django Project Structure

---
+++ codebuzz/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for codebuzz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codebuzz.settings")
+
+application = get_wsgi_application()
